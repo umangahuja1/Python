981563cade55497322f013486a8f81b7ce0cba35
Umang Ahuja <umangahuja1@gmail.com>
2017-05-31 20:51:15 +0530

    Create search_2.py

---
+++ automation/search_2.py
@@ -0,0 +1,18 @@
+from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Chrome() 
+driver.get('http://www.google.com/xhtml');
+print('Browser open...')
+sleep(2) 
+search_box = driver.find_element_by_name('q')
+query=input('enter the thing you want to search:')
+search_box.send_keys(query)
+sleep(2)
+search_box.submit()
+print('Searched...')
+sleep(5) 
+print('Bye Bye.')
+sleep(2)
+driver.quit()
+print('Game Over')
