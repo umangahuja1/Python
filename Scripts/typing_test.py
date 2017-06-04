2fa6beb1c08ef8b45a08f429954105d5b958722b
Umang Ahuja <umangahuja1@gmail.com>
2017-06-05 03:47:29 +0530

    Update typing_test.py

--- Scripts/typing_test.py
+++ Scripts/typing_test.py
@@ -1,13 +1,14 @@
 from time import time
 
 print()
-print("NO NEW LINE IS THERE WRITE CONTINUOUSLY,just SPACES")
+print("NO NEW LINE IS THERE, WRITE CONTINUOUSLY(just SPACES)")
 s="this is a simple paragraph that is meant to be nice and easy to type which is why there will be no commas no periods or any capital letters so i guess this means that it cannot really be considered a paragraph but just a series of sentences"
+words=(len(s.split()))
 
 print()
 print(s)
 
-print("\nAfter you are done press enter to know your time")
+print("\nAfter you are done press enter to know your time and speed")
 input("\nPress any key to Start:")
 
 try:
@@ -16,8 +17,11 @@ try:
 	t=input()
 	end=time()
 	if(t==s):
+		total=round(end-start,2)
 		print("\nVoila you typed that correctly")
-		print("Your time was %s"%round(end-start,2))
+		print("Your time was %s seconds"%total)
+		total=int(total)/60
+		print("Speed was %s wpm"%(str(words//total)))
 
 	else: 
 		print("\nWrongly entered")
