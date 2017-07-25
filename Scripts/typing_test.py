7ea331d4005b27c448a6d5720d919eca24de2eac
Umang Ahuja <umangahuja1@gmail.com>
2017-07-25 11:15:26 +0530

    Update typing_test.py

--- Scripts/typing_test.py
+++ Scripts/typing_test.py
@@ -1,13 +1,13 @@
-'''
-This is a simple script to cehck typing speed
-'''
-
+""" This is a simple script to test your typing speed """
 from time import time
 
 print()
 print("NO NEW LINE IS THERE, WRITE CONTINUOUSLY(just SPACES)")
-s="this is a simple paragraph that is meant to be nice and easy to type which is why there will be no commas no periods or any capital letters so i guess this means that it cannot really be considered a paragraph but just a series of sentences"
-words=(len(s.split()))
+s = "this is a simple paragraph that is meant to be nice and" \
+    " easy to type which is why there will be no commas no periods " \
+    "or any capital letters so i guess this means that it cannot really " \
+    "be considered a paragraph but just a series of sentences"
+words = (len(s.split()))
 
 print()
 print(s)
@@ -16,20 +16,20 @@ print("\nAfter you are done press enter to know your time and speed")
 input("\nPress any key to Start:")
 
 try:
-	print("\nTimer Started\n")
-	start=time()
-	t=input()
-	end=time()
-	if(t==s):
-		total=round(end-start,2)
-		print("\nVoila you typed that correctly")
-		print("Your time was %s seconds"%total)
-		total=int(total)/60
-		print("Speed was %s wpm"%(str(words//total)))
+    print("\nTimer Started\n")
+    start = time()
+    t = input()
+    end = time()
+    if t == s:
+        total = round(end - start, 2)
+        print("\nVoila you typed that correctly")
+        print("Your time was %s seconds" % total)
+        total = int(total) / 60
+        print("Speed was %s wpm" % (str(words // total)))
 
-	else: 
-		print("\nWrongly entered")
-		print("Try again")
+    else:
+        print("\nIncorrectly entered")
+        print("Try again")
 
 except KeyboardInterrupt:
-	print("")
+    print("")
